9d203e70bbab2bca65758130e6eaf8b7a82d6c85
june <thanks7229@naver.com>
2025-07-01 17:18:53 +0900

    docs: README.md

---
+++ src/main/java/com/metabus/gittest3/Gittest3Application.java
@@ -0,0 +1,13 @@
+package com.metabus.gittest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittest3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gittest3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/metabus/gittest3/Gittest3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metabus.gittest3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gittest3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
